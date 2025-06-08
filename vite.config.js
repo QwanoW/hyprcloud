d2414744fe81e5246224cabe90c1192ce865a5ec
QwanoW <domocristalix@gmail.com>
2025-06-09 02:50:34 +0500

    feat: add gzip and brotli compression to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import laravel from 'laravel-vite-plugin';
-import {
-    defineConfig
-} from 'vite';
-import tailwindcss from "@tailwindcss/vite";
 import i18n from 'laravel-react-i18n/vite';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
     plugins: [
@@ -16,6 +15,22 @@ export default defineConfig({
         react(),
         tailwindcss(),
         i18n(),
+        compression({
+            algorithm: 'gzip',
+            ext: '.gz',
+            threshold: 10240, // минимальный размер файла в байтах для сжатия (10 KiB)
+            deleteOriginFile: false, // оставить оригинальные файлы
+        }),
+        // второй экземпляр — Brotli
+        compression({
+            algorithm: 'brotliCompress',
+            ext: '.br',
+            threshold: 10240, // тоже от 10KiB
+            brotliOptions: {
+                level: 11, // максимальный уровень сжатия
+            },
+            deleteOriginFile: false,
+        }),
     ],
     esbuild: {
         jsx: 'automatic',
